eeea741bed4d82a17f7c6ca474e99b35bd082153
mariemmohamedshawky <mariemmohamed154@gmail.com>
2021-03-25 10:15:26 -1000

    initial commit

---
+++ lib/constants/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//colors used in app
+
+const Color primary = Color(0xFFba181b);
+const Color secondary = Color(0xFFf3f2f3);
+const Color white = Colors.white;
+const Color black = Colors.black;
+const Color grey = Colors.grey;
