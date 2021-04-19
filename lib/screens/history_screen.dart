d5ff9a614ddf7702be38546cc724d735f3710cac
mariemmohamedshawky <mariemmohamed154@gmail.com>
2021-04-18 16:51:53 -1000

    some edit in History-Screen

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -4,7 +4,12 @@ import 'package:flutter/material.dart';
 class HistoryScreen extends StatefulWidget {
   static const String routeName = 'history-screen';
   HistoryScreen({Key key}) : super(key: key);
-  final List<String> sampleList = <String>['', '', '', '', ''];
+  final List<String> sampleList = <String>[
+    "first card info,first card info,first card info,first card info,first card info,first card info,first card info,first card info",
+    "second card info,second card info,second card info second card info,second card info,second cari,nfo",
+    "second card info,second card info,second card info second card info,second card info,second card info",
+    "third card info,third card info,third card info,third card info,third card info,"
+  ];
 
   @override
   _HistoryScreenState createState() => _HistoryScreenState();
@@ -46,18 +51,26 @@ class _HistoryScreenState extends State<HistoryScreen> {
         ),
         body: Column(
           children: [
-            SizedBox(height: 50),
+            SizedBox(height: 30),
             Expanded(
               child: ListView.builder(
                 itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    elevation: 4.0,
-                    child: Padding(
-                      child: Text(
-                        '${widget.sampleList[index]}',
-                        style: TextStyle(fontSize: 10.0),
+                  return Container(
+                    height: 120,
+                    child: Card(
+                      shadowColor: Color(0xFFf25757),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      padding: EdgeInsets.all(20.0),
+                      elevation: 6,
+                      margin: EdgeInsets.all(5),
+                      child: Padding(
+                        child: Text(
+                          '${widget.sampleList[index]}',
+                          style: TextStyle(fontSize: 10.0),
+                        ),
+                        padding: EdgeInsets.all(20.0),
+                      ),
                     ),
                   );
                 },
